eb75e16da80d0c8a08af08cdb201b9d4b08845aa
Hieu Vo <hieuvo044@gmail.com>
2025-01-06 07:48:48 +0700

    fix footer error

--- src/app/layouts/projects/projects.component.ts
+++ src/app/layouts/projects/projects.component.ts
@@ -4,50 +4,78 @@ import { Project } from 'src/app/models/project';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent {
   projects: Project[] = [
     {
-      name: "eShopicaOnContainer",
-      description: "🤖 Microservices project follow by NET-Microservices-Architecture-for-Containerized-NET-Applications Document.",
+      name: 'eShopicaOnContainer',
+      description:
+        '🤖 Microservices project follow by N-tier architecture & NET-Microservices-Architecture-for-Containerized-NET-Applications Document.',
       technologies: ['.NET', 'Docker', 'RabbitMQ', 'Azure Service Bus'],
-      repoLink: "https://dev.azure.com/shopica/_git/ShopicaOnContainer"
+      repoLink: 'https://dev.azure.com/shopica/_git/ShopicaOnContainer',
     },
     {
-      name: "CI/CD-Movie",
-      description: "🚀 Using Github Actions to automate testing, building and deploying their applications to an existing Kubernetes cluster.",
+      name: 'CI/CD-Movie',
+      description:
+        '🚀 Using Github Actions to automate testing, building and deploying their applications to an existing Kubernetes cluster.',
       technologies: ['React', 'TypeScript', 'Flask', 'Python', 'Kubernetes'],
-      repoLink: "https://github.com/HieuVo1/CICD-Movie"
+      repoLink: 'https://github.com/HieuVo1/CICD-Movie',
     },
     {
-      name: "hvt-portfolio",
-      description: "🔥 Personal Portfolio project using Github Actions deploy to AWS S3.",
+      name: 'wts-erp',
+      description:
+        '🔥 ERP software system for internal company. Including all the core business  proscesses need to run a company like finance, HR, manufacturing, supply chain, services, procurement, and others.',
+      technologies: [
+        '.NET',
+        'EF Core',
+        '.NET Aspire',
+        'N-tier architecture',
+        'PostgreSQL',
+        'React',
+        'Material UI',
+        'TypeScript',
+      ],
+      repoLink: '',
+    },
+    {
+      name: 'cocauru',
+      description:
+        '🔥 Support build CI/CD, IaC for ecommerce application instead of manual deployment for 4 hours. Add AWS EventBridge to decrease cost for 10 EC2 Crawler run on EC2',
+      technologies: ['AWS EC2', 'Terraform', 'AMI', 'Laravel', 'MySQL'],
+      repoLink: '',
+    },
+    {
+      name: 'hvt-portfolio',
+      description:
+        '🔥 Personal Portfolio project using Github Actions deploy to AWS S3.',
       technologies: ['Angular', 'Ng-Zorro', 'TypeScript', 'CI/CD', 'AWS S3'],
-      repoLink: "https://github.com/HieuVo1/hvt-portfolio"
+      repoLink: 'https://github.com/HieuVo1/hvt-portfolio',
     },
 
     {
-      name: "Azure-AI",
-      description: "🎉 Research project with Azure AI Services like Document Intelligence, Speech-to-text,Translation,...",
+      name: 'Azure-AI',
+      description:
+        '🎉 Research project with Azure AI Services like Document Intelligence, Speech-to-text,Translation,...',
       technologies: ['.NET', 'C#', 'Azure AI Services'],
-      repoLink: "https://github.com/HieuVo1/Azure-AI"
+      repoLink: 'https://github.com/HieuVo1/Azure-AI',
     },
     {
-      name: "shopica-client",
-      description: "📍A eCommerce website for clothing using Github Actions deploy to AWS S3.",
+      name: 'shopica-client',
+      description:
+        '📍A eCommerce website for clothing using Github Actions deploy to AWS S3.',
       technologies: ['Angular', 'Ng-Zorro', '.NET', 'SQLServer', 'AWS'],
-      repoLink: "https://shopica-client.hvtauthor.com/"
+      repoLink: 'https://shopica-client.hvtauthor.com/',
     },
     {
-      name: "shopica-recommender-system",
-      description: "🤖 Recommendation API for eCommerce website for clothing.",
+      name: 'shopica-recommender-system',
+      description: '🤖 Recommendation API for eCommerce website for clothing.',
       technologies: ['Django', 'MySQL', 'scikit-surprise'],
-      repoLink: "https://github.com/HieuVo1/shopica-recommender-system"
-    }
-  ]
+      repoLink: 'https://github.com/HieuVo1/shopica-recommender-system',
+    },
+  ];
 
   goToLink(url: string) {
-    window.open(url, "_blank");
+    if (url) window.open(url, '_blank');
   }
 }
--- src/app/layouts/skills/skills.component.ts
+++ src/app/layouts/skills/skills.component.ts
@@ -3,92 +3,97 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent {
   skills: Skill[] = [
     {
-      name: 'HTML',
-      imageUrl: 'assets/images/skills/html5-original-wordmark.svg'
-    },
-    {
-      name: 'CSS',
-      imageUrl: 'assets/images/skills/css3-original-wordmark.svg'
-    },
-    {
-      name: 'JavaScript',
-      imageUrl: 'assets/images/skills/javascript-original.svg'
-    },
-    {
-      name: 'TypeScript',
-      imageUrl: 'assets/images/skills/typescript-original.svg'
-    },
-    {
-      name: 'Angular',
-      imageUrl: 'assets/images/skills/angular.svg'
-    },
-    {
-      name: 'Ng-Zorro',
-      imageUrl: 'assets/images/skills/ng-zorro.svg'
-    },
-    {
       name: '.NET',
-      imageUrl: 'assets/images/skills/dot-net-original-wordmark.svg'
+      imageUrl: 'assets/images/skills/dot-net-original-wordmark.svg',
     },
     {
       name: 'EF Core',
-      imageUrl: 'assets/images/skills/ef-core.png'
+      imageUrl: 'assets/images/skills/ef-core.png',
     },
     {
+      name: 'HTML',
+      imageUrl: 'assets/images/skills/html5-original-wordmark.svg',
+    },
+    {
+      name: 'CSS',
+      imageUrl: 'assets/images/skills/css3-original-wordmark.svg',
+    },
+    {
+      name: 'JavaScript',
+      imageUrl: 'assets/images/skills/javascript-original.svg',
+    },
+    {
+      name: 'TypeScript',
+      imageUrl: 'assets/images/skills/typescript-original.svg',
+    },
+    {
+      name: 'Angular',
+      imageUrl: 'assets/images/skills/angular.svg',
+    },
+    {
+      name: 'React',
+      imageUrl: 'assets/images/skills/react-logo.png',
+    },
+    {
+      name: 'Ng-Zorro',
+      imageUrl: 'assets/images/skills/ng-zorro.svg',
+    },
+
+    {
       name: 'SQL Server',
-      imageUrl: 'assets/images/skills/microsoft-sql-server-logo.svg'
+      imageUrl: 'assets/images/skills/microsoft-sql-server-logo.svg',
     },
     {
       name: 'MongoDB',
-      imageUrl: 'assets/images/skills/mongodb-original-wordmark.svg'
+      imageUrl: 'assets/images/skills/mongodb-original-wordmark.svg',
     },
 
     {
       name: 'AWS',
-      imageUrl: 'assets/images/skills/amazonwebservices-original-wordmark.svg'
+      imageUrl: 'assets/images/skills/amazonwebservices-original-wordmark.svg',
     },
     {
       name: 'Azure',
-      imageUrl: 'assets/images/skills/microsoft_azure-icon.svg'
+      imageUrl: 'assets/images/skills/microsoft_azure-icon.svg',
     },
     {
       name: 'Redis',
-      imageUrl: 'assets/images/skills/redis-original-wordmark.svg'
+      imageUrl: 'assets/images/skills/redis-original-wordmark.svg',
     },
     {
       name: 'RabbitMQ',
-      imageUrl: 'assets/images/skills/rabbitmq-icon.svg'
+      imageUrl: 'assets/images/skills/rabbitmq-icon.svg',
     },
     {
       name: 'Docker',
-      imageUrl: 'assets/images/skills/docker-original-wordmark.svg'
+      imageUrl: 'assets/images/skills/docker-original-wordmark.svg',
     },
     {
       name: 'Kubernetes',
-      imageUrl: 'assets/images/skills/kubernetes-icon.svg'
+      imageUrl: 'assets/images/skills/kubernetes-icon.svg',
     },
     {
       name: 'Miroservies',
-      imageUrl: 'assets/images/skills/icon-microservices.png'
+      imageUrl: 'assets/images/skills/icon-microservices.png',
     },
     {
       name: 'Git',
-      imageUrl: 'assets/images/skills/git-scm-icon.svg'
+      imageUrl: 'assets/images/skills/git-scm-icon.svg',
     },
     {
       name: 'Figma',
-      imageUrl: 'assets/images/skills/figma-icon.svg'
+      imageUrl: 'assets/images/skills/figma-icon.svg',
     },
     {
       name: 'Business Central',
-      imageUrl: 'assets/images/skills/business-central-logo.png'
+      imageUrl: 'assets/images/skills/business-central-logo.png',
     },
-  ]
+  ];
 }
 export interface Skill {
   name: string;
